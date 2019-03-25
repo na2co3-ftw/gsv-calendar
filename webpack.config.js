55ba7c1006d469d44145a8fb80307892a29181f1
na2co3 <na2co3.soda@gmail.com>
2019-03-26 00:09:43 +0900

    Add cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,42 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-	mode: "development",
-	entry: [
-		path.join(__dirname, "app/ui/index.tsx")
-	],
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: ["ts-loader"]
-			}
-		]
-	},
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"]
-	},
-	output: {
-		path: path.join(__dirname, "out"),
-		filename: "scripts.js"
+module.exports = (env, argv) => {
+	const PRODUCTION = argv.mode === "production";
+	const plugins = [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "app/ui/index.template.html"
+		})
+	];
+	if (PRODUCTION) {
+		plugins.push(
+			new CleanWebpackPlugin()
+		);
 	}
+
+	return {
+		mode: "development",
+		entry: [
+			path.join(__dirname, "app/ui/index.tsx")
+		],
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: ["ts-loader"]
+				}
+			]
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		output: {
+			path: path.join(__dirname, "out"),
+			filename: PRODUCTION ? "scripts.[chunkhash:10].js" : "scripts.js"
+		},
+		plugins
+	};
 };
